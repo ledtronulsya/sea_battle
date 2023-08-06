e80d2d05a0605d70d2b3b80946792de93b9c7d03
ledtronulsya <ostapus12@gmail.com>
2023-08-06 21:14:26 +0300

    dummy for user and ai added

--- core/board.py
+++ core/board.py
@@ -1,22 +1,22 @@
 from dot import Dot
 from ship import Ship
 from exceptions import *
-from helper_classes import DotSymbol, Direction, SYMBOLS_DICT
+from helper_classes import DotSymbol, Direction
 
 
 class Board:
     """Класс игрового поля"""
     def __init__(self, is_hidden: bool = False, rows: int = 6, columns: int = 6) -> None:
         self.is_hidden = is_hidden
-        self.rows, self.columns = rows + 1, columns + 1
+        self.rows, self.columns = rows, columns
         self.field = [[Dot(i, j) for j in range(columns + 1)] for i in range(rows + 1)]
         self.add_headers()
         self.ships = []
 
     def add_headers(self):
         """Добавляет чисел на первой строке и первом столбце"""
-        self.field[0] = [" "] + list(range(1, self.columns))
-        for row in range(1, self.rows):
+        self.field[0] = [" "] + list(range(1, self.columns + 1))
+        for row in range(1, self.rows + 1):
             self.field[row][0] = row
 
     def add_ship(self, ship: Ship):
@@ -32,37 +32,36 @@ class Board:
             self.field[dot.x][dot.y].mark(symb)
     
     @staticmethod
-    def get_neightbour_dots(dot: Dot, ship: Ship) -> tuple[Dot, Dot]:
-        """Возвраащет соседние точки"""
-        dot1 = Dot(dot.x - 1 * ship.dir.horizontal, dot.y - 1 * ship.dir.vertical)
-        dot2 = Dot(dot.x + 1 * ship.dir.horizontal, dot.y + 1 * ship.dir.vertical)
+    def get_neightbour_dots(dot: Dot, dr: Direction, ax: int = 0) -> tuple[Dot, Dot]:
+        """Возвраащет соседние точки. Если ax=0, возвращет две соседних, 
+        не соответствующих направлению корабля (если корабль вертикальный, вернет соседей по горизонтали).
+        Если ax=1, вернет точки, соответствущих направлению корабля"""
+        if ax == 0:
+            dot1 = Dot(dot.x - 1 * dr.horizontal, dot.y - 1 * dr.vertical)
+            dot2 = Dot(dot.x + 1 * dr.horizontal, dot.y + 1 * dr.vertical)
+        elif ax == 1:
+            dot1 = Dot(dot.x - 1 * dr.vertical, dot.y - 1 * dr.horizontal)
+            dot2 = Dot(dot.x + 1 * dr.vertical, dot.y + 1 * dr.horizontal)
         return dot1, dot2
 
     def contour(self, ship: Ship):
         """Помечает точки вокруг корабля, в которые нельзя ставить другие корабли"""
         dots = ship.dots()
+        dots_around = []
         for dot in ship.dots():
             self.set_symbol(dot, DotSymbol.ship)
-            dot1, dot2 = self.get_neightbour_dots(dot, ship)
-            self.set_symbol(dot1, DotSymbol.around)
-            self.set_symbol(dot2, DotSymbol.around)
-        doth = Dot(dots[0].x - 1 * ship.dir.vertical, dots[0].y - 1 * ship.dir.horizontal)
-        doth1, doth2 = self.get_neightbour_dots(doth, ship)
-        self.set_symbol(doth, DotSymbol.around)
-        self.set_symbol(doth1, DotSymbol.around)
-        self.set_symbol(doth2, DotSymbol.around)
-        doth = Dot(dots[-1].x + 1 * ship.dir.vertical, dots[-1].y + 1 * ship.dir.horizontal)
-        doth1, doth2 = self.get_neightbour_dots(doth, ship)
-        self.set_symbol(doth, DotSymbol.around)
-        self.set_symbol(doth1, DotSymbol.around)
-        self.set_symbol(doth2, DotSymbol.around)
+            dots_around.extend(self.get_neightbour_dots(dot, ship.dir))
+        doth = self.get_neightbour_dots(dots[0], ship.dir, ax=1)[0]
+        dott = self.get_neightbour_dots(dots[-1], ship.dir, ax=1)[1]
+        dots_around.extend([doth, dott, *self.get_neightbour_dots(doth, ship.dir), *self.get_neightbour_dots(dott, ship.dir)])
+        [self.set_symbol(d, DotSymbol.around) for d in dots_around]
 
     @property
-    def ships_alive(self):
+    def ships_alive(self) -> int:
         """Возвращает число живых кораблей"""
         return len([ship for ship in self.ships if ship.is_alive])
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Вывод доски в консоль"""
         return '\n'.join([" | ".join(map(str, row)) + " |" for row in self.field])
     
@@ -71,13 +70,13 @@ class Board:
         return not (1 <= dot.x <= self.rows and 1 <= dot.y <= self.columns)
     
     def find_ship_with_dot(self, dot: Dot) -> Ship | None:
-        """Ищет корабль, имеющие точку dot"""
+        """Ищет корабль, имеющий точку dot"""
         for ship in self.ships:
             if dot in ship.dots():
                 return ship
         return None
 
-    def shot(self, dot: Dot):
+    def shot(self, dot: Dot) -> bool:
         """Делает выстрел"""
         if self.out(dot):
             raise OutOfBoundException(dot)
@@ -96,7 +95,8 @@ s = Ship(3, Dot(2, 2), Direction(vertical=True))
 b.add_ship(s)
 #print(b)
 b.shot(Dot(2, 2))
-b.shot(Dot(2, 3))
+b.shot(Dot(3, 2))
+b.shot(Dot(3, 2))
 print(b)
         
 
--- core/player.py
+++ core/player.py
@@ -1,10 +1,32 @@
-class Player:
-    """Класс пользователя"""
-    def __init__(self):
-        pass
+from board import Board
+from random import randint
+from dot import Dot
 
-    def ask(self):
+
+class Player:
+    """Класс игрока"""
+    def __init__(self, player_board: Board, opponent_board: Board, show_errors: bool = True):
+        self.player_board = player_board
+        self.opponent_board = opponent_board
+        self.show_errors = show_errors
+
+    def ask(self) -> Dot:
         """Спрашивет игрока, в какую клеетку произвести выстрел"""
     
     def move(self):
+        while True:
+            
+
+
+class User(Player):
+    """Класс пользователя"""
+
+    def ask(self):
         pass
+
+
+class AI(Player):
+    """Класс игрового бота"""
+
+    def ask(self) -> Dot:
+        return Dot(randint(1, self.opponent_board.rows), randint(1, self.opponent_board.columns))
