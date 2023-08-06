77fec3167dd8c634674642a6d85263b0829c4b96
ledtronulsya <ostapus12@gmail.com>
2023-08-07 00:03:37 +0300

    player class modified

--- core/player.py
+++ core/player.py
@@ -1,27 +1,39 @@
 from board import Board
 from random import randint
 from dot import Dot
+from exceptions import *
 
 
 class Player:
     """Класс игрока"""
-    def __init__(self, player_board: Board, opponent_board: Board, show_errors: bool = True):
+    def __init__(self, player_board: Board, opponent_board: Board, log: bool = True):
         self.player_board = player_board
         self.opponent_board = opponent_board
-        self.show_errors = show_errors
+        self.log = log
 
     def ask(self) -> Dot:
         """Спрашивет игрока, в какую клеетку произвести выстрел"""
     
     def move(self):
         while True:
-            
+            try:
+                dot = self.ask()
+                result = self.opponent_board.shot(dot)
+                if not result:
+                    break
+                elif self.log:
+                    print("Вы можете сделать еще один ход")
+            except (OutOfBoundException, AlreadyMarkedDotException) as error:
+                if self.log:
+                    print(error)
 
 
 class User(Player):
     """Класс пользователя"""
 
-    def ask(self):
+    def ask(self) -> Dot:
+        x, y = map(int, input("Введите строку и столбец ячейки через пробел: ").split())
+        return Dot(x, y)
         pass
 
 
